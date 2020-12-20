f5c09056a4b032c96af7cea6b53ceac1e981cc7d
okba bekhelifi <okba.bekhelifi@gmail.com>
2020-12-20 14:54:58 +0100

    - Added SWLDA classifier to baseline ERP  On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	new file:   baseline/erp/stepwise/README
    	new file:   baseline/erp/stepwise/__init__.py
    	new file:   baseline/erp/stepwise/stepwise.py
    	new file:   baseline/erp/swlda.py

---
+++ baseline/erp/swlda.py
@@ -0,0 +1,42 @@
+# Author: Okba Bekhelifi
+
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.metrics import accuracy_score
+from sklearn.utils.extmath import softmax
+from .stepwise.stepwise import stepwisefit
+import numpy as np
+
+class SWLDA(BaseEstimator, ClassifierMixin): 
+	
+	# Step-wise LDA for binary classification.
+	# uses stepwisefit.py by Collin RM Stocks
+	
+    def __init__(self):
+        self.w = None
+        self.b = None
+        self.inmodel = None
+    
+    def fit(self, X, y=None, penter=0.1, premove=0.15):
+        b, se, pval, inmodel, stats, nextstep, history = stepwisefit(X, y, penter=0.1, premove=0.15)
+        x1 = X[y==1,:]
+        mu1 = x1.mean(axis=0)
+        x2 = X[y==0,:] 
+        mu2 = x2.mean(axis=0)
+        mu_both = (mu1 + mu2) / 2 
+        self.w = np.zeros(b.shape)
+        self.w[inmodel] = b[inmodel]
+        self.b = np.dot(-self.w.transpose(), mu_both)        
+        self.inmodel = inmodel
+        return self
+    
+    def decision_function(self,X):
+        return np.dot(X,self.w) + self.b
+    
+    def predict(self, X, y=None):
+        return  np.sign(self.decision_function(X))   
+    
+    def score(self,X, y=None):
+        return accuracy_score(y,self.predict(X))
+    
+    def predict_proba(self,X):
+        return softmax(self.decision_function(X))
